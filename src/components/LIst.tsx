bff00618679ef240f19015963c15eaccad7b41af
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-10-26 14:43:32 -0300

    refac: add dark mode colors

--- src/components/LIst.tsx
+++ src/components/LIst.tsx
@@ -7,7 +7,7 @@ interface ListProps {
 
 export function List({ personList }: ListProps) {
   return (
-    <ul className='w-full h-full flex flex-col gap-2 p-2 pr-4 overflow-y-auto overflow-x-hidden scrollbar-thumb-zinc-500 hover:scrollbar-thumb-zinc-600 scrollbar-track-zinc-300  scrollbar-thin'>
+    <ul className='w-full h-full flex flex-col gap-2 p-2 pr-4 overflow-y-auto overflow-x-hidden scrollbar-thin scrollbar-thumb-zinc-500 hover:scrollbar-thumb-zinc-600 scrollbar-track-zinc-300 dark:scrollbar-thumb-zinc-300 dark:hover:scrollbar-thumb-zinc-200 dark:scrollbar-track-zinc-600'>
       {personList.map((person: RandomUserResult, index: number) => (
         <ListItem key={index} person={person} />
       ))}
--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -8,17 +8,17 @@ interface ListItemProps {
 function ListItem({ person }: ListItemProps) {
   return (
     <li className='w-full h-24 animate-fadeIn'>
-      <div className='w-full h-full flex flex-row items-center rounded-lg border bg-gray-100 overflow-hidden hover:border-gray-300 hover:bg-gray-200  hover:drop-shadow-md transition-all'>
+      <div className='w-full h-full flex flex-row items-center rounded-lg border bg-gray-100 overflow-hidden hover:border-gray-300 hover:translate-y-[-2px]  hover:drop-shadow-md transition-all relative dark:bg-zinc-700 dark:border-zinc-600'>
         <img
           src={person.picture.large}
           alt={`${person.name.first}' profile picture`}
           className='w-32 h-32 relative left-[-1.2rem] rounded-full'
         />
         <div>
-          <p className='text-gray-900'>
+          <p className='text-gray-900 dark:text-gray-50'>
             {person.name.first} {person.name.last}
           </p>
-          <p className='text-gray-600'>{person.email}</p>
+          <p className='text-gray-600 dark:text-gray-400'>{person.email}</p>
         </div>
       </div>
     </li>
