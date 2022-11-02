044839bd8c77566408c30295a81268a69f3bd372
Diego <contato.diegoluisdasilva@gmail.com>
2022-11-02 12:07:03 -0300

    refac: adjusting max size and footer

--- src/App.tsx
+++ src/App.tsx
@@ -46,49 +46,51 @@ export function App() {
   }, []);
 
   return (
-    <div className='w-screen h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-zinc-900'>
-      <input
-        className='w-3/4 h-12 min-w-[350px] p-2 border rounded text-gray-900 dark:border-zinc-600 dark:bg-zinc-800 dark:text-gray-50 disabled:opacity-75'
-        placeholder='Localizar contato...'
-        type='search'
-        disabled={personList.length <= 0}
-        value={personSearch}
-        onChange={(
-          event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-        ) => {
-          setPersonSearch(event.target.value);
-        }}
-      />
-      <div className='w-3/4 min-w-[350px] h-[500px] mt-2 rounded-lg bg-gray-50 dark:bg-zinc-800 drop-shadow-2xl relative overflow-auto'>
-        {personList && personList.length > 0 ? (
-          shownSkeleton ? (
-            <SkeletonList />
+    <div className='w-screen h-screen flex flex-col items-center justify-center overflow-auto bg-gray-100 dark:bg-zinc-900'>
+      <main className='w-full max-w-3xl h-full flex flex-1 flex-col gap-2 items-center justify-center'>
+        <input
+          className='w-3/4 h-12  p-2 relative border rounded text-gray-900 dark:border-zinc-600 dark:bg-zinc-800 dark:text-gray-50 disabled:opacity-75'
+          placeholder='Localizar contato...'
+          type='search'
+          disabled={personList.length <= 0}
+          value={personSearch}
+          onChange={(
+            event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+          ) => {
+            setPersonSearch(event.target.value);
+          }}
+        />
+        <div className='w-3/4 h-[500px]  rounded-lg bg-gray-50 dark:bg-zinc-800 drop-shadow-2xl relative overflow-auto'>
+          {personList && personList.length > 0 ? (
+            shownSkeleton ? (
+              <SkeletonList />
+            ) : (
+              <List personList={filteredPersonList} />
+            )
           ) : (
-            <List personList={filteredPersonList} />
-          )
-        ) : (
-          <SkeletonList />
-        )}
-      </div>
-      <button
-        type='button'
-        className={`w-44 p-2 m-2 rounded font-bold shadow-md border transition-all ${
-          shownSkeleton
-            ? "bg-green-500 border-green-500 text-zinc-900 hover:bg-green-600"
-            : "bg-zinc-900 hover:bg-zinc-800 dark:border-green-500 dark:text-green-500 hover:dark:bg-green-900"
-        }
+            <SkeletonList />
+          )}
+        </div>
+        <button
+          type='button'
+          className={`w-44 p-2 m-2 rounded font-bold shadow-md border relative transition-all ${
+            shownSkeleton
+              ? "bg-green-500 border-green-500 text-zinc-900 hover:bg-green-600"
+              : "bg-zinc-900 hover:bg-zinc-800 dark:border-green-500 dark:text-green-500 hover:dark:bg-green-900"
+          }
         `}
-        onClick={() => {
-          setShownSkeleton(!shownSkeleton);
-        }}
-      >
-        Mostrar {shownSkeleton ? "Lista" : "Skeleton"}
-      </button>
-      <footer className='w-full h-12 flex flex-col items-center justify-center relative bottom-0 bg-gray-100 dark:bg-zinc-700 dark:border-t-zinc-600'>
+          onClick={() => {
+            setShownSkeleton(!shownSkeleton);
+          }}
+        >
+          Mostrar {shownSkeleton ? "Lista" : "Skeleton"}
+        </button>
+      </main>
+      <footer className='w-full h-12 flex flex-col items-center justify-center overflow-hidden  bg-gray-100 dark:bg-zinc-700 dark:border-t-zinc-600'>
         <p className='text-white'>
-          Projeto desenvolvido por{" "}
+          Desenvolvido por{" "}
           <a href='https://github.com/diegodls' target='_blank'>
-            <strong className='text-green-500 hover:text-green-300'>
+            <strong className='text-green-500 transition-all hover:text-green-300'>
               Diego
             </strong>
           </a>
